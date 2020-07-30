9e1eb9668f8f6bfea5d85b8b704d6562434f56d1
Your-github-name <youremail@gmail.com>
2020-07-30 13:37:32 +0530

    committed on Thu, Jul 30, 2020  1:37:30 PM

---
+++ wp-content/uploads/custom-css-js/209231.js
@@ -0,0 +1,12 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+function myFunction() {
+  var x = document.getElementById("successform");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
+<!-- end Simple Custom CSS and JS -->
